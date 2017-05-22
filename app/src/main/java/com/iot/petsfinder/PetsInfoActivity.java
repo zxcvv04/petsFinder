82430d800fc61251b73a3f7f8b9d14037c1e40e4
구윤영 <dbsdud0413@gmail.com>
2017-05-22 14:39:29 +0900

    version = "0.07";

---
+++ app/src/main/java/com/iot/petsfinder/PetsInfoActivity.java
@@ -0,0 +1,15 @@
+package com.iot.petsfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PetsInfoActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pets_info);
+    }
+}
