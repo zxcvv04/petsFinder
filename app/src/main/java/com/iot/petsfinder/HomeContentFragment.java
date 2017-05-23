db4941dfa975ce68c21e1ebd768fabd5e996cf5c
ehdgh9772 <ehdgh9772@naver.com>
2017-05-23 16:55:58 +0900

    Version="0.24"

--- app/src/main/java/com/iot/petsfinder/HomeContentFragment.java
+++ app/src/main/java/com/iot/petsfinder/HomeContentFragment.java
@@ -1,7 +1,129 @@
 package com.iot.petsfinder;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomeContentFragment extends Fragment
 {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.activity_home, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView image;
+        public TextView type;
+        public TextView age;
+        public TextView gender;
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_view, parent, false));
+            image = (ImageView) itemView.findViewById(R.id.img);
+            type = (TextView) itemView.findViewById(R.id.type);
+            age = (TextView) itemView.findViewById(R.id.age);
+            gender = (TextView) itemView.findViewById(R.id.gender);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, Detail.class);
+                    intent.putExtra(Detail.EXTRA_POSITION, getAdapterPosition());
+                    context.startActivity(intent);
+                }
+            });
+
+            // Adding Snackbar to Action Button inside card
+            Button button = (Button)itemView.findViewById(R.id.action_button);
+            button.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, "Action is pressed",
+                            Snackbar.LENGTH_LONG).show();
+                }
+            });
+
+            ImageButton favoriteImageButton =
+                    (ImageButton) itemView.findViewById(R.id.favorite_button);
+            favoriteImageButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, "Added to Favorite",
+                            Snackbar.LENGTH_LONG).show();
+                }
+            });
+
+            ImageButton shareImageButton = (ImageButton) itemView.findViewById(R.id.share_button);
+            shareImageButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, "Share article",
+                            Snackbar.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    /**
+     * Adapter to display recycler view.
+     */
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of Card in RecyclerView.
+        private static final int LENGTH = 18;
+        private final Drawable[] image;
+        private final String[] type;
+        private final String[] age;
+        private final String[] gender;
+
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            type = resources.getStringArray(R.array.places);
+            age = resources.getStringArray(R.array.place_details);
+            gender = resources.getStringArray(R.array.place_desc);
+            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
+            image = new Drawable[a.length()];
+            for (int i = 0; i < image.length; i++) {
+                image[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.image.setImageDrawable(image[position % image.length]);
+            holder.type.setText(type[position % type.length]);
+            holder.age.setText(age[position % age.length]);
+            holder.gender.setText(gender[position % gender.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
+    }
 }
