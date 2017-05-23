e9a3f05b94098fe2d01cbb11ddfc0e4cd4d7298d
ehdgh9772 <ehdgh9772@naver.com>
2017-05-23 15:50:47 +0900

    Version="0.22"

--- app/src/main/java/com/iot/petsfinder/MainActivity.java
+++ app/src/main/java/com/iot/petsfinder/MainActivity.java
@@ -2,6 +2,7 @@ package com.iot.petsfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
-        TabLay tabs = (TabLayout) findViewById(R.id.tabs);
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
 
         textView01 = (TextView) findViewById(R.id.userMode);
