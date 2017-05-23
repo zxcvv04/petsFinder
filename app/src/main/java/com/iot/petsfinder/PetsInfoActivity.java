1ea51f76302031d4714845c828a833831d3bc3e1
ehdgh9772 <ehdgh9772@naver.com>
2017-05-23 10:43:19 +0900

    Version="0.19"

--- app/src/main/java/com/iot/petsfinder/PetsInfoActivity.java
+++ app/src/main/java/com/iot/petsfinder/PetsInfoActivity.java
@@ -1,15 +1,49 @@
 package com.iot.petsfinder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class PetsInfoActivity extends AppCompatActivity
 {
+    private RecyclerView _recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pets_info);
+
+        _recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        ArrayList<Album> albums = loadData();
+
+        AlbumAdapter adapter = new AlbumAdapter(albums);
+        _recyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        _recyclerView.setLayoutManager(layoutManager);
+        _recyclerView.setItemAnimator(new DefaultItemAnimator());
+    }
+
+    private ArrayList<Album> loadData()
+    {
+        ArrayList<Album> albums = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            Album album = new Album();
+            album.setImageId(R.drawable.dogsub2);
+            album.setType("종류"+(i+1));
+            album.setAge((i+1));
+            album.setGender("성별"+"남");
+
+            albums.add(album);
+        }
+        return albums;
     }
 }
