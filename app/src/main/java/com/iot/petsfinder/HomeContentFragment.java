eb3a8f877fee059bcfc6414e2fd2275c09ecace8
ehdgh9772 <ehdgh9772@naver.com>
2017-05-24 11:35:06 +0900

    Version="0.30"

--- app/src/main/java/com/iot/petsfinder/HomeContentFragment.java
+++ app/src/main/java/com/iot/petsfinder/HomeContentFragment.java
@@ -6,26 +6,21 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomeContentFragment extends Fragment
 {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
-                R.layout.activity_pets_info, container, false);
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.activity_pets_info, container, false);
         ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
@@ -56,51 +51,13 @@ public class HomeContentFragment extends Fragment
                     context.startActivity(intent);
                 }
             });
-
-            // Adding Snackbar to Action Button inside card
-            Button button = (Button) itemView.findViewById(R.id.action_button);
-            button.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    Snackbar.make(v, "Action is pressed",
-                            Snackbar.LENGTH_LONG).show();
-                }
-            });
-
-            ImageButton favoriteImageButton =
-                    (ImageButton) itemView.findViewById(R.id.favorite_button);
-            favoriteImageButton.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    Snackbar.make(v, "Added to Favorite",
-                            Snackbar.LENGTH_LONG).show();
-                }
-            });
-
-            ImageButton shareImageButton = (ImageButton) itemView.findViewById(R.id.share_button);
-            shareImageButton.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    Snackbar.make(v, "Share article",
-                            Snackbar.LENGTH_LONG).show();
-                }
-            });
         }
     }
 
-    /**
-     * Adapter to display recycler view.
-     */
     public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder>
     {
         // Set numbers of Card in RecyclerView.
-        private static final int LENGTH = 18;
+        private static final int LENGTH = 1;
 
         private final String[] mPlaces;
         private final String[] mPlaceDesc;
