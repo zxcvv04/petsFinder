b76f015651f65ce01234b13375950ed983d345af
ehdgh9772 <ehdgh9772@naver.com>
2017-05-25 15:31:29 +0900

    Version="0.49"

--- app/src/main/java/com/iot/petsfinder/LostListActivity.java
+++ app/src/main/java/com/iot/petsfinder/LostListActivity.java
@@ -2,60 +2,46 @@ package com.iot.petsfinder;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-
-public class LoserInfoActivity extends AppCompatActivity
+public class LostListActivity extends AppCompatActivity
 {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private CardView _cardView;
+    private RecyclerView _recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loser_info);
+        setContentView(R.layout.activity_pets_info);
 
-        /*mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        _recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
 
-        ArrayList<Album> albums = loadData();
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        _recyclerView.setLayoutManager(layoutManager);
+        _recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        AlbumAdapter adapter = new AlbumAdapter(albums);
-        mRecyclerView.setAdapter(adapter);
+        _cardView=(CardView) findViewById(R.id.card_view);
 
-        // 이 부분에서 정렬 방식을 설정합니다.
-        mLayoutManager = new GridLayoutManager(this, 2);
-        //mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        mAdapter = new AlbumAdapter(albums);
-       mRecyclerView.setAdapter(mAdapter);*/
-
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL));
-        ArrayList<Album> albums = loadData();
-        AlbumAdapter adapter = new AlbumAdapter(albums);
-        recyclerView.setAdapter(adapter);
-
-    }
-
-    private ArrayList<Album> loadData()
-    {
-        ArrayList<Album> albums = new ArrayList<>();
-
-        for (int i = 0; i < 100; i++)
-        {
-            Album album = new Album();
-            album.setImageId(R.drawable.dogsub2);
-            album.setType("종류"+(i+1));
-            album.setAge((i+1));
-            album.setGender("성별"+"남");
-
-            albums.add(album);
-        }
-        return albums;
+        _recyclerView.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        _recyclerView.setClickable(true);
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "항목이 선택되었습니다.",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                }
+        );
     }
 }
